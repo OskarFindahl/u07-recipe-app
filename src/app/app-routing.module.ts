9b1e805d20c0b8fe3e62d228066bbe8858be5f8c
Oskar Findahl <oskar_findahl@hotmail.com>
2021-05-27 21:40:58 +0200

    Add login and register routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShowRecipesComponent } from './show-recipes/show-recipes.component';
 import { ShowRecipesDetailsComponent } from './show-recipes-details/show-recipes-details.component';
-import { UserRecipesListComponent } from './user-recipes-list/user-recipes-list.component'
+import { UserRecipesListComponent } from './user-recipes-list/user-recipes-list.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: SigninComponent },
+  { path: 'register', component: SignupComponent },
   { path: 'ShowRecipes', component: ShowRecipesComponent },
   { path: 'ShowRecipesDetails/:id', component: ShowRecipesDetailsComponent },
   { path: 'UserRecipesList', component: UserRecipesListComponent },
