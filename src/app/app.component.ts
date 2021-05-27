bc101eb9ecd8b871dc30cfe742961e4c64c499a6
Oskar Findahl <oskar_findahl@hotmail.com>
2021-05-27 21:42:51 +0200

    import routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RecipeService } from './recipe.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { TokenService } from './shared/token.service';
+import { AuthStateService } from './shared/auth-state.service';
+
 
 @Component({
   selector: 'app-root',
@@ -8,18 +12,26 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'u07-recipe-app';
+  isSignedIn: boolean;
 
   valMem:string;
   dietMem: string;
   recipes: Observable<any>;
   constructor(
     private RecipeService: RecipeService,
+    private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService,
+
     ) {
   }
   ngOnInit() {
     this.recipes = this.RecipeService.getRecipes('main-corse', '');
+    this.auth.userAuthState.subscribe(val => {
+      this.isSignedIn = val;
+  });
   }
 
   mealType(value, diet) {
