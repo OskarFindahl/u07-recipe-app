4e609be0499cb55423b4bd6b88822793cdfa913b
Oskar Findahl <oskar_findahl@hotmail.com>
2021-05-28 02:10:04 +0200

    Connect to AuthService for addToList function

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, concat, empty, forkJoin } from 'rxjs';
 import { environment } from '../environments/environment';
+import { AuthService } from './shared/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,11 @@ export class RecipeService {
 
   ListRecipes: Observable<any>;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private AuthService: AuthService,
+    ) {}
+
   getRecipes(mealType, diet): Observable<any> {
     //
     return this.http.get<any>(`https://api.spoonacular.com/recipes/complexSearch?type=${mealType}&diet=${diet}&apiKey=${environment.API_KEY}`);
@@ -33,6 +38,14 @@ export class RecipeService {
       this.ListIDs.push(recipe.slice(0, index));
       this.ListINames.push(recipe.slice(index+1));
     }
+
+
+    //This is for the API-List connection 
+    this.AuthService.addToList(recipe.slice(0, index), recipe.slice(index+1)).subscribe((data:any) => {
+      console.log(data);
+    });
+    
+
   }
 
   RemoveFromList(index): any{
